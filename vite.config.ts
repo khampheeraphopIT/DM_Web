0211d96e435ff6774f87c8cba8fee75fb5d731ae
khampheeraphop <poplowplay1@gmail.com>
2025-11-12 16:56:16 +0700

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true, // อนุญาตให้ทุก host เข้าถึงใน dev
     port: 5173,
-    open: true,
-    host: true,
+  },
+  preview: {
+    host: true, // อนุญาตทุก host
+    port: parseInt(process.env.PORT || "4173"),
+    allowedHosts: ["caneapi.onrender.com"], // เพิ่ม host ของคุณที่ Render
   },
 });
